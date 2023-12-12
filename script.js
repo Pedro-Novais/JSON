830f870af67fb9a06de61bfd414aee0f27937a91
Pedro-Novais <phnovais7@gmail.com>
2023-12-12 14:20:07 -0300

    First json

---
+++ script.js
@@ -0,0 +1,26 @@
+const div = document.querySelector('#container')
+
+getJson()
+function getJson(){
+    let requestURL = "actions.json";
+
+    let request = new XMLHttpRequest()
+
+    request.open("GET", requestURL);
+
+    request.responseType = "json";
+    request.send();
+
+    request.onload = function () {
+        let profile = request.response;
+        view(profile)
+    };
+}
+
+function view(profile){
+    let name = document.createTextNode(profile.name)
+    console.log(profile)
+    div.appendChild(name)
+}
+
+
