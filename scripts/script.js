35e8c07fe109d36e2b8d9111a935324f3e20e751
Pedro-Novais <phnovais7@gmail.com>
2024-01-04 19:14:22 -0300

    Finishing

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ import deleteTask from "./deleteXML.js"
 import updateTaskBack from "./updateXML.js"
 import order from "./orderPriority.js"
 //import finishTaskBack from "./finishTask.js"
-import {modalEdit, modalRemovetext} from "./modals.js"
+import { modalEdit, modalRemovetext } from "./modals.js"
 
 const api = 'http://localhost:5000/tasks'
 const apiConfig = 'http://localhost:5000/config'
@@ -16,64 +16,64 @@ getPriority()
 const btn = document.querySelector('#btn-add')
 const task_put = document.querySelector('#task-add')
 
-if(task_put){
-  task_put.addEventListener('click', () =>{
+if (task_put) {
+  task_put.addEventListener('click', () => {
     task_put.removeAttribute('class', 'watch-out')
   })
 }
-if(btn){
+if (btn) {
   btn.addEventListener('click', addTask)
 }
 
 let priorityId = null;
-function addTask(){
+function addTask() {
   const agora = new Date();
   const minutos = agora.getMinutes();
   const sec = agora.getSeconds();
   console.log(`${agora.getHours()}:${minutos}:${sec}`)
   const input = document.querySelector('#task-add').value
 
-  if(priorityId == null){
+  if (priorityId == null) {
     priorityId = 0
   }
-  if(input == "" || input == null){
+  if (input == "" || input == null) {
     task_put.setAttribute('class', 'watch-out')
     return false
   }
   priorityId = priorityId + 1
   let val = task_put.value
-  let data= {
+  let data = {
     task: val,
     priority: priorityId
   }
-    addTaskBack(api, data)
+  addTaskBack(api, data)
 }
 
-function loadingConfig(configs){
+function loadingConfig(configs) {
   let priorityOrderJson = configs["orderPriority"]
 
   loadingTask(priorityOrderJson)
 }
 
-function loadingTask(priorityJson){
-  if (priorityJson == true){
-    for(let i = 3; i >= 1; i--){
+function loadingTask(priorityJson) {
+  if (priorityJson == true) {
+    for (let i = 3; i >= 1; i--) {
       order(api, orderPriority, i)
     }
-  }else{
+  } else {
     order(api, orderPriority)
   }
 }
 
-function orderPriority(order){
-  if(order.length > 0){
-    for(let i = 0; i < order.length; i++){
+function orderPriority(order) {
+  if (order.length > 0) {
+    for (let i = 0; i < order.length; i++) {
       createTask(order, i)
     }
   }
 }
 
-function createTask(taskJson, index){
+function createTask(taskJson, index) {
   const container = document.querySelector('#task-made')
 
   const div = document.createElement('div')
@@ -88,7 +88,7 @@ function createTask(taskJson, index){
   container.appendChild(div)
 }
 
-function createNameTask(json, i, div){
+function createNameTask(json, i, div) {
   const p = document.createElement('p')
   p.classList.add('task-puted')
 
@@ -98,12 +98,12 @@ function createNameTask(json, i, div){
   div.appendChild(p)
 }
 
-function createPriorityTask(json, div, priority,id){
+function createPriorityTask(json, div, priority, id) {
   const container = document.createElement('div')
   container.setAttribute('class', 'priority-task')
   let level = []
   let priJson = json[id]["priority"]
-  for(let i = 0; i < 3; i++){
+  for (let i = 0; i < 3; i++) {
     level[i] = document.createElement('div')
     level[i].setAttribute('class', 'priority-level')
 
@@ -111,13 +111,13 @@ function createPriorityTask(json, div, priority,id){
     priority.appendChild(container)
   }
 
-    for(let i = 0; i < priJson; i++){
-        level[i].style.backgroundColor='#d6ac5e';
-    }
-    div.appendChild(priority)
+  for (let i = 0; i < priJson; i++) {
+    level[i].style.backgroundColor = '#d6ac5e';
+  }
+  div.appendChild(priority)
 }
 
-function createActions(div, priority,i, json){
+function createActions(div, priority, i, json) {
   let id = json[i]['id']
   const div_action = document.createElement('div')
   div_action.setAttribute('class', 'container-actions')
@@ -153,13 +153,13 @@ function createActions(div, priority,i, json){
   priority.appendChild(div_action)
   div.appendChild(priority)
 
-  i_add.addEventListener('click', () =>{
-    modalRemove(json, id)
+  i_add.addEventListener('click', () => {
+    modalRemove(json, id, 1)
     validUpdate(id, 2)
   })
 
   i_del.addEventListener('click', () => {
-    modalRemove(json, id)
+    modalRemove(json, id, 0)
     validUpdate(id, 1)
   })
 
@@ -169,50 +169,61 @@ function createActions(div, priority,i, json){
   })
 
 }
-function modalRemove(json, id){
+function modalRemove(json, id, determinate) {
   let modalDiv = document.createElement('div');
   modalDiv.setAttribute('class', 'modal')
   modalDiv.setAttribute('id', 'modal-remove')
 
   const body = document.querySelector('body')
   const container = document.querySelector('#container')
-  container.style.display="none"
+  container.style.display = "none"
 
   modalDiv.innerHTML = modalRemovetext
 
   body.appendChild(modalDiv)
+
+  const titleAction = document.querySelector('#title-determinate')
+
+  if (determinate == 0) {
+    let title = document.createTextNode("Excluir tarefa")
+    titleAction.appendChild(title)
+  }
+  if (determinate == 1) {
+    let title = document.createTextNode("Concluir tarefa")
+    titleAction.appendChild(title)
+  }
   modalRemovePriority(json, id)
 }
 
-function modalRemovePriority(json, id){
+function modalRemovePriority(json, id) {
   const levelMark = document.querySelectorAll('[level]')
   let nameTask;
   let levelTask;
-  
-  for(let i = 0; i <= json.length; i++){
-    if(id == json[i]["id"]){
+
+  for (let i = 0; i <= json.length; i++) {
+    if (id == json[i]["id"]) {
       nameTask = json[i]["task"]
       levelTask = json[i]["priority"]
       break
     }
   }
- 
+
   let textTask = document.querySelector('#task-remove-text')
   let text = document.createTextNode(nameTask)
 
   textTask.appendChild(text)
 
-  for(let i = 0; i < levelTask; i++){
-    levelMark[i].style.backgroundColor="#d6ac5e"
+  for (let i = 0; i < levelTask; i++) {
+    levelMark[i].style.backgroundColor = "#d6ac5e"
   }
 }
-function modalChange(){
+function modalChange() {
   let modalDiv = document.createElement('div');
   modalDiv.setAttribute('class', 'modal')
   modalDiv.setAttribute('id', 'modal-edit')
   const levelPiority = document.querySelectorAll('.choose-priority')
 
-  for(let i = 0; i < levelPiority.length; i++){
+  for (let i = 0; i < levelPiority.length; i++) {
     levelPiority[i].getAttribute('id')
     levelPiority[i].removeAttribute('id')
     levelPiority[i].removeAttribute('class')
@@ -221,8 +232,8 @@ function modalChange(){
   const body = document.querySelector('body')
 
   const container = document.querySelector('#container')
-  container.style.display="none"
- 
+  container.style.display = "none"
+
   modalDiv.innerHTML = modalEdit;
 
   body.appendChild(modalDiv)
@@ -230,94 +241,97 @@ function modalChange(){
 
 }
 
-function validUpdate(id, determinate){
+function validUpdate(id, determinate) {
   const btnNewTask = document.querySelector('#btn-edit-edit')
   const btnCancelNewTask = document.querySelector('#btn-edit-cancel')
 
-    btnNewTask.addEventListener('click', function(event){
-      if(determinate == 0){
-        const inputTaskElement = document.querySelector('#input-edit')
-        const inputTask = document.querySelector('#input-edit').value
-        event.preventDefault()
-        if(inputTask == ""){
-          inputTaskElement.style.boxShadow = "rgba(216, 34, 18, 0.863) 0px 0px 0px 2px, rgba(228, 5, 5, 0.65) 0px 4px 6px -1px, rgba(153, 26, 26, 0.08) 0px 1px 0px inset"
-          inputTaskElement.addEventListener("click", ()=>{
-            inputTaskElement.style.boxShadow="none"
-          })
-          return false
-        }else{
-          updateTask(id, inputTask)
-        }
-        }else if(determinate == 1){
-          deleteTask(api, id)
-        }else if(determinate == 2){
-          //finishiTask(api, id)
-          console.log("indo")
-        }
-    })
-
-    btnCancelNewTask.addEventListener('click', ()=>{
-      let priority = ["priority-one", "priority-two", "priority-three"]
-      let modalType = ".modal"
-      let divModal = document.querySelector(modalType)
-      divModal.remove();
-
-      const container = document.querySelector('#container')
-      container.style.display="flex"
-
-      if(determinate == 0){
-        const levelPiority = document.querySelectorAll('[mark]')
-
-        for(let i = 0; i < levelPiority.length; i++){
-          levelPiority[i].setAttribute('id', priority[i])
-          levelPiority[i].setAttribute('class', 'choose-priority')
-        }
-      }
+  btnNewTask.addEventListener('click', function (event) {
+    if (determinate == 0) {
+      const inputTaskElement = document.querySelector('#input-edit')
+      const inputTask = document.querySelector('#input-edit').value
+      event.preventDefault()
+      if (inputTask == "") {
+        inputTaskElement.style.boxShadow = "rgba(216, 34, 18, 0.863) 0px 0px 0px 2px, rgba(228, 5, 5, 0.65) 0px 4px 6px -1px, rgba(153, 26, 26, 0.08) 0px 1px 0px inset"
+        inputTaskElement.addEventListener("click", () => {
+          inputTaskElement.style.boxShadow = "none"
         })
+        return false
+      } else {
+        updateTask(id, inputTask)
+      }
+    } else if (determinate == 1) {
+      deleteTask(api, id)
+    } else if (determinate == 2) {
+      finishTask(id)
+    }
+  })
+
+  btnCancelNewTask.addEventListener('click', () => {
+    let priority = ["priority-one", "priority-two", "priority-three"]
+    let modalType = ".modal"
+    let divModal = document.querySelector(modalType)
+    divModal.remove();
+
+    const container = document.querySelector('#container')
+    container.style.display = "flex"
+
+    if (determinate == 0) {
+      const levelPiority = document.querySelectorAll('[mark]')
+
+      for (let i = 0; i < levelPiority.length; i++) {
+        levelPiority[i].setAttribute('id', priority[i])
+        levelPiority[i].setAttribute('class', 'choose-priority')
+      }
+    }
+  })
 }
 
-function updateTask(id, newTask){
-  let data={
+function finishTask(id) {
+  console.log('indo')
+}
+
+function updateTask(id, newTask) {
+  let data = {
     task: newTask,
     priority: priorityId + 1
   }
   updateTaskBack(api, id, data)
 }
 
-function getPriority(){
+function getPriority() {
   const level = document.querySelectorAll('.choose-priority')
-  level.forEach(function(level) {
-    level.addEventListener('click', function() {
-        clearNewClick()
-        priorityId = level.getAttribute('id');
-        if(priorityId == "priority-one"){
-          priorityId = 0
-        }
-        else if(priorityId == "priority-two"){
-          priorityId = 1
-        }
-        else if(priorityId == "priority-three"){
-          priorityId = 2
-        }
+  level.forEach(function (level) {
+    level.addEventListener('click', function () {
+      clearNewClick()
+      priorityId = level.getAttribute('id');
+      if (priorityId == "priority-one") {
+        priorityId = 0
+      }
+      else if (priorityId == "priority-two") {
+        priorityId = 1
+      }
+      else if (priorityId == "priority-three") {
+        priorityId = 2
+      }
 
-        if(priorityId !== null){
-          markNewClick(priorityId)
-        }
-        console.log(priorityId)
+      if (priorityId !== null) {
+        markNewClick(priorityId)
+      }
+      console.log(priorityId)
     });
   });
 }
 
-function markNewClick(index){
+function markNewClick(index) {
   let level = document.querySelectorAll('.choose-priority')
-  for(let i = 0; i <= index; i++){
-      level[i].style.backgroundColor='#d6ac5e';
+  for (let i = 0; i <= index; i++) {
+    level[i].style.backgroundColor = '#d6ac5e';
   }
 }
 
-function clearNewClick(){
+function clearNewClick() {
   let level = document.querySelectorAll('.choose-priority')
-  for(let i = 0; i < level.length; i++){
-      level[i].style.backgroundColor='#6acfc9';
+  for (let i = 0; i < level.length; i++) {
+    level[i].style.backgroundColor = '#6acfc9';
   }
 }
