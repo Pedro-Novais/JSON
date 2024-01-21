c3813201b294c64f17f41bad3242054c0341de98
Pedro-Novais <phnovais7@gmail.com>
2024-01-21 12:04:53 -0300

    Criando o Back end

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,6 +53,56 @@ const userController = {
     delete: async (req, res) => {
         try {
 
+            const id = req.params.id
+
+            const user = await UserModel.findById(id)
+
+            if (!user) {
+                res.status(404).json({ msg: "Usúario não encontrado" })
+                return
+            }
+
+            const deleteUser = await UserModel.findByIdAndDelete(id)
+
+            res
+                .status(200)
+                .json({ deleteUser, msg: "Usúario excluido com sucesso" })
+
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    update: async (req, res) => {
+        try {
+
+            const id = req.params.id
+            let updatePatch = {};
+           
+            if (req.body.name) {
+                updatePatch.name = req.body.name
+            } 
+            if (req.body.email) {
+                updatePatch.email = req.body.email
+            }
+            if (req.body.password) {
+                updatePatch.password = req.body.password
+            }
+            if (req.body.description) {
+                updatePatch.description = req.body.description
+            }
+
+            const updateUser = await UserModel.findByIdAndUpdate(id, updatePatch)
+
+            if (!updateUser) {
+                res.status(404).json({ msg: "Usúario não encontrado" })
+                return
+            }
+
+            res
+                .status(200)
+                .json({ updatePatch, msg: "Atualização feita com sucesso" })
+
         } catch (error) {
             console.log(error)
         }
