841c77efbd6a881612ec3e59060ebfc2b58b787e
Pedro-Novais <phnovais7@gmail.com>
2024-05-21 16:25:08 -0300

    Mudar a pasta de views para dentro da pasta public

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Configuração do mecanismo de template
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 
 // DB Conection
 const conn = require('./backend/db/conn')
