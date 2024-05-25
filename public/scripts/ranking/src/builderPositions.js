f3dad08a6c330858cfee033eb90ede55a2f5fdbb
Pedro-Novais <phnovais7@gmail.com>
2024-05-25 14:36:45 -0300

    Criar componente builder_positions

--- public/scripts/ranking/src/builderPositions.js
+++ public/scripts/ranking/src/builderPositions.js
@@ -1,6 +1,7 @@
 import { get_json } from '../../utils/functionsReq.js'
 import {get_token } from '../../utils/getToken.js'
 import { API } from '../../utils/endPoints.js'
+import { PopUpGlobal } from '../../utils/popup_global.js'
 
 export class BuilderPositions {
 
@@ -16,6 +17,66 @@ export class BuilderPositions {
 
         const response = await get_json(API.url_ranking, token)
 
-        console.log(response)
+        if(!response.ok){
+
+            console.error('Algum erro ocorreu ao carregar o ranking')
+            new PopUpGlobal('#container-ranking', 'Erro!', 'Algum erro ocorreu ao carregar o ranking!')
+
+            return false
+        }
+
+        this.verify_users(response.responseData)
+    }
+
+    verify_users(users){
+
+        console.log(users)
+
+        if (users.ranking.length === 0){
+
+            console.warn('Nenhum usuário está no ranking')
+
+            return false
+        }
+  
+        users.ranking.forEach(element => {
+            
+            this.builder_positions(element)
+        });
+    }
+
+    builder_positions(user){
+
+        const container_ranking = document.querySelector('.users-ranking')
+
+        const position = document.createElement('div')
+        position.setAttribute('class', 'position-users')
+        position.setAttribute('position', user.position) 
+        
+        const classBox = ["box-position", "box-name", "box-points"]
+        const valueBox = [`${user.position}º`, user.nameUser, `${user.tasksFinished} pts`]
+
+        for (let i = 0; i < 3; i++) {
+
+            const infos = this.insert_infos(i, classBox, valueBox)
+
+            position.appendChild(infos)
+
+        }
+
+        container_ranking.appendChild(position)
+    }
+
+    insert_infos(index, class_name, value ){
+
+        const div = document.createElement('div')
+
+        div.setAttribute('class', class_name[index])
+        div.setAttribute('title', 'Visualizar perfil')
+
+        div.innerHTML = value[index]
+
+        return div
+
     }
 }
